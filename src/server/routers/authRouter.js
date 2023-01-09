c0d2b966d489681111aa7543750f48f06976f77b
GrantMelvin <Grantmelvin4@gmail.com>
2023-01-09 15:51:15 -0500

    edited db names

--- src/server/routers/authRouter.js
+++ src/server/routers/authRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const client = require("../databases/DB");
+const client = require("../database/DB");
 const bcrypt = require("bcrypt");
 
 const validateForm = require("../controllers/validateForm");
