27a8da6c12ec634ea904d644790bcb11907f9d89
GrantMelvin <Grantmelvin4@gmail.com>
2023-12-09 12:00:13 -0500

    Database connected!

--- backend/database/DB.js
+++ backend/database/DB.js
@@ -1,11 +1,17 @@
 const {Client} = require('pg')
  
+const caCert = 'rds-ca-2019'
+
 const client = new Client({
     host: "ecuquestcs.c8ixdx9wngdk.us-east-1.rds.amazonaws.com",
     user: "postgres",
     port: 5432,
     password: "Leposa13",
-    database: "ecuquestcs"
+    database: "initdb",
+    ssl: {
+        rejectUnauthorized: false,
+        ca: caCert,
+    },
 })  
 
 module.exports = client ;
